257dbd2989f97f70ef9621f76b9387924d46c84a
krupsKSM <krupasindhu.dev@gmail.com>
2025-07-10 20:06:30 +0530

    Refactor: Modularize Todo app into TodoInput and TodoList components

--- create-TODO-app/TODO/src/App.jsx
+++ create-TODO-app/TODO/src/App.jsx
@@ -1,10 +1,34 @@
 import AppTest from "./components/AppTest"
+import React, {useState} from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
 const App = () => {
+
+    const [todos, setTodos] = useState([])
+
+    const handleAdd = (task) => {
+        setTodos(prev => [task, ...prev])
+    }
     return (
         <>
             <h1>hi</h1>
             {/* AppTest component used for dry run */}
-            <AppTest />   
+            {/* <AppTest />    */}
+
+            <h2>To-Do App</h2>
+            <TodoInput onAdd = {handleAdd}/>   
+            {/* now onAdd gets task from TodoInput.jsx child (lifting state up) */}
+           
+            {/* <div>
+                <ul>
+                    {todos.map((item,index)=>(
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div> not required once we use TodoList component  */}
+
+            <TodoList todos = {todos} />
         </>
     )
 }
