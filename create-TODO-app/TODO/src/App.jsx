fe628dea5a81432ceb04f8033c39517a20fbef61
krupsKSM <krupasindhu.dev@gmail.com>
2025-07-11 01:28:42 +0530

    feat(delete): Add delete task feature to todo list

--- create-TODO-app/TODO/src/App.jsx
+++ create-TODO-app/TODO/src/App.jsx
@@ -5,10 +5,19 @@ import TodoList from "./components/TodoList"
 
 const App = () => {
 
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([])  // todos -> array of Objects {id, task}
 
     const handleAdd = (task) => {
-        setTodos(prev => [task, ...prev])
+        const newTodo = {
+            id: Date.now(),
+            task, //  this means task: task
+        }
+        setTodos(prev => [newTodo, ...prev])
+    }
+
+    const handleDelete = (id) =>{
+        const filtered = todos.filter(todo => todo.id !== id)
+        setTodos(filtered)
     }
     return (
         <>
@@ -28,7 +37,7 @@ const App = () => {
                 </ul>
             </div> not required once we use TodoList component  */}
 
-            <TodoList todos = {todos} />
+            <TodoList todos = {todos} onDelete= {handleDelete}/>
         </>
     )
 }
