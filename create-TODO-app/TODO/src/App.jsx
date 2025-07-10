c49b82512e69c103863dceaccd746723e3a6e317
krupsKSM <krupasindhu.dev@gmail.com>
2025-07-10 16:39:42 +0530

    Add test file to check code viability

---
+++ create-TODO-app/TODO/src/App.jsx
@@ -0,0 +1,11 @@
+import AppTest from "./components/AppTest"
+const App = () => {
+    return (
+        <>
+            <h1>hi</h1>
+            {/* AppTest component used for dry run */}
+            <AppTest />   
+        </>
+    )
+}
+export default App
